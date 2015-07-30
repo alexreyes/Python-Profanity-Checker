282e12c3c1924eae0320344b408e691fd55a5981
Alex <reyesruizalex@gmail.com>
2015-07-30 11:58:09 -0400

    Make the result more clear.

--- check_profanity.py
+++ check_profanity.py
@@ -3,14 +3,19 @@ import urllib
 def read_text():
 	quotes = open("test_file.txt")
 	contents_of_file = quotes.read()
-	print(contents_of_file)
 	quotes.close()
 	check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
 	connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
 	output = connection.read()
-	print(output)
+	
+	if "true" in output:
+		print("There is profanity in your file.")
+	elif "false" in output:
+		print("There is not profanity in your file.")
+	else: 
+		print("I couldn't read the file properly.")
 	connection.close()
 
 read_text()
